5be5744c9a34b12662f96d8c51a01a78e31b046e
wangfl <wangfl@parox.cn>
2015-12-08 14:57:47 +0800

    sql update && mapper fix

--- doc/console_alter.sql
+++ doc/console_alter.sql
@@ -1,7 +1,9 @@
 
-/* 2015-11-25 线上未加 */
+/* 2015-11-25 */
 alter table orders add column `name_setup` varchar(20) Default '' COMMENT '需装者姓名' after `status`;
 alter table orders add column `phone_setup` varchar(20) Default '' COMMENT '需装者电话' after `name_setup`;
 alter table orders add column `addr_setup` varchar(45) Default '' COMMENT '安装地址' after `phone_setup`;
+
+alter table orders drop column `cid` ;
 
 drop table customer;
